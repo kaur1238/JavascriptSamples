3dcdd02901bd41581616b16cc3783fea655decf2
kaur1238 <harpriyakaur03@gmail.com>
2017-11-08 19:54:26 -0500

    Adding project

---
+++ php/job.php
@@ -0,0 +1,34 @@
+
+<html>
+<head>
+    <link rel="stylesheet" href="../newcss.css"
+</head>
+<body background="../images/bck.jpg" >
+    <div id="wrapper">
+        <div id="main-nav">
+            <ul>
+                <li><a  href="../index.html">Home</a></li>
+                <li><a href="../portfolio.html">Portfolios</a></li>
+                <li><a href="../packages.html">Packages</a></li>
+                <li><a href="../special.html">Special Offers</a></li>
+                <li><a href="../feedback.html">Customer Feedback</a></li>
+                <li><a class="selected" href="../jobs.html">Jobs</a></li>
+                <li><a href="../contact.html">Contact Us</a></li>
+            </ul>
+        </div>
+        <div id="footer">
+            <div id="altnav">
+                <a href="../index.html">Home</a> -
+                <a href="../portfolio.html">Portfolios</a> -
+                <a href="../packages.html">Packages</a> -
+                <a href="../special.html">Special Offers</a> -
+                <a href="../feedback.html">Customer Feedback</a> -
+                <a href="../jobs.html">Jobs</a> -
+                <a href="../contact.html">Contact Us</a> 
+            </div>
+
+            Copyright &copy;Capturing Moments<br>
+            Visit my site: <a href="index.html" target="_blank">Capturing-Moments</a>
+        </div>
+</body>
+</html>
